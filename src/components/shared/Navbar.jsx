d13003563e11554040f118f6d888770283c2aad2
Maliha Afroj Orna <63250354+Maliha-Afroj-Orna@users.noreply.github.com>
2026-07-29 22:37:42 +0600

    place the navbar text and avatar icon

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,7 +1,33 @@
+import Link from "next/link";
 import React from "react";
+import userAvatar from "@/assets/user.png";
+import Image from "next/image";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link href="">Home</Link>
+        </li>
+        <li>
+          <Link href="">About</Link>
+        </li>
+        <li>
+          <Link href="">Career</Link>
+        </li>
+      </ul>
+
+      <div>
+        <Image
+          src={userAvatar}
+          alt="User Avatar"
+          height={60}
+          width={60}
+        ></Image>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
