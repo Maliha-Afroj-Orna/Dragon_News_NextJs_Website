c9ad133922bbfae1ac42e76334bce34155b066ad
orna <orna@atilimited.net>
2026-07-30 12:06:27 +0600

    display categories and design right sidebar

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,3 +1,6 @@
+import LeftSidebar from "@/components/homepage/news/LeftSidebar";
+import RightSidebar from "@/components/homepage/news/RightSidebar";
+
 async function getCategories() {
   const res = await fetch(
     "https://openapi.programming-hero.com/api/news/categories",
@@ -12,25 +15,13 @@ export default async function Home() {
   return (
     <div className="grid grid-cols-12 gap-4 container mx-auto my-[60px]">
       <div className="col-span-3">
-        <h2 className="font-bold text-lg">All Categories</h2>
-        <ul className="flex flex-col gap-3 mt-6">
-          {categories.map((category) => {
-            return (
-              <li
-                key={category.category_id}
-                className="bg-slate-100 p-2 rounded-md font-bold text-center text-md"
-              >
-                {category.category_name}
-              </li>
-            );
-          })}
-        </ul>
+        <LeftSidebar categories={categories} activeId={null}></LeftSidebar>
       </div>
       <div className="font-bold text-3xl bg-purple-100 col-span-6">
         All News
       </div>
-      <div className="font-bold text-3xl bg-yellow-100 col-span-3">
-        Social Icons
+      <div className="col-span-3">
+        <RightSidebar></RightSidebar>
       </div>
     </div>
   );
