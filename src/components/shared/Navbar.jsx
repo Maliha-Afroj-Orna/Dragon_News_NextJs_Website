28d5c14b47076c7d82b61f2a5252afc2a2153829
orna <orna@atilimited.net>
2026-07-30 10:14:36 +0600

    design navbar section

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -2,29 +2,36 @@ import Link from "next/link";
 import React from "react";
 import userAvatar from "@/assets/user.png";
 import Image from "next/image";
+import NavLink from "./NavLink";
 
 const Navbar = () => {
   return (
-    <div>
-      <ul>
+    <div className="flex justify-between container mx-auto gap-4 mt-6">
+      <div></div>
+      <ul className="flex justify-between items-center text-gray-700 gap-3">
         <li>
-          <Link href="">Home</Link>
+          <NavLink href="/" className="text-purple-500">
+            Home
+          </NavLink>
         </li>
         <li>
-          <Link href="">About</Link>
+          <NavLink href="/about-us">About</NavLink>
         </li>
         <li>
-          <Link href="">Career</Link>
+          <NavLink href="/career">Career</NavLink>
         </li>
       </ul>
 
-      <div>
+      <div className="flex items-center gap-2">
         <Image
           src={userAvatar}
           alt="User Avatar"
           height={60}
           width={60}
         ></Image>
+        <button className="btn bg-purple-500 text-white">
+          <Link href={"/login"}>Login</Link>
+        </button>
       </div>
     </div>
   );
